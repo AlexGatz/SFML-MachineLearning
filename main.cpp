98284a641e5596d9df7bc306b023ee7a57572a68
TheNeoCoder <the.neocoder@gmail.com>
2019-01-17 11:28:31 -0500

    Tail loop working to create and draw new shapes, next I need to point each new drawn shape at 'snake head' and somehow lock their positions to the head

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace sf;
 #define PI 3.14159265
 
 constexpr int windowWidth{800}, windowHeight{600};
-constexpr float snakeSize{30.f}, snakePoints{5};
+constexpr float snakeSize{30.f}, snakePoints{3};
 
 class Util
 {
@@ -24,9 +24,9 @@ public:
 
 	// From [-180, 180] to [0, 360]
 	if(desiredAngle < 0)
-	    {
-		desiredAngle = 360 - (desiredAngle);
-	    }
+	{
+	    desiredAngle = 360 - (desiredAngle);
+	}
 	
 	// Gives the option to offset the angle for use with different shapes.
 	desiredAngle += offset;
@@ -40,44 +40,95 @@ class Snake
 private:
 
     Vector2f velocity;
-    Vector2i mousePos;
     float angle;
-    int nodes;
     Util util;
     Texture texture;
+    Color snakeColor;
+    float startPosX;
+    float adjustedPosX;
+    float startPosY;
+    int numberOfNodes;
+    CircleShape snake;
+    vector<CircleShape> snakeNodes;
+    
+    // Looking into using ternary to construct tail from vector of snakes
+    void snakeTail()
+    {
+	cout << "snakeTail() called." << "\n";
+	cout << "Number of nodes: " << numberOfNodes << "\n";
+	// Step through and set values for each snakeNode
+
+	// Initialize
+	snakeNodes.resize(numberOfNodes);
+
+	cout << "allocate memory for snakeNodes" << "\n";
+	adjustedPosX = startPosX;
+
+	cout << "adjustedPosX: " << adjustedPosX << "\n";
+	
+        for(int i = 0; i < numberOfNodes; i++)
+	{
+	    cout << "for loop working..." << "\n";
+	    // Need to adjust start pos by some amount
+	    adjustedPosX += 10;
+
+	    cout << "new pos: " << adjustedPosX << "\n";
+	
+	    snakeNodes[i].setPosition(adjustedPosX, startPosY);
+	    snakeNodes[i].setRadius(snakeSize);
+	    snakeNodes[i].setPointCount(snakePoints);
+	    snakeNodes[i].setFillColor(snakeColor);
+	    snakeNodes[i].setOrigin(snakeSize, snakeSize);
+	    //snakeNodes[i].setTexture(&snakeTexture);
+	    // each origin should be fixed to the previous origin
+	    // add snake node to list and set each angle to follow the previous node
+	}
+    }
     
 public:
     
-    CircleShape shape;
-    
-    Snake(float mx, float my, Color idk)
+    // Snake constructor
+    Snake(float mx, float my, Color color, int nodes)
     {
-	if(!texture.loadFromFile("test.png"))
-	    {
-		cout << "Did not load image\n";
-	    }
+	snakeColor = color;
+	numberOfNodes = nodes;	
+	startPosX = mx;
+	startPosY = my;
 	
-	shape.setPosition(mx, my);
-	shape.setRadius(snakeSize);
-	shape.setPointCount(snakePoints);
-	//	shape.setFillColor(Color::Green);
-	shape.setFillColor(idk);
-	shape.setOrigin(snakeSize, snakeSize);
-	shape.setTexture(&texture);
+	// if(!texture.loadFromFile("someImage.png"))
+	// {
+	//     cout << "Did not load image\n";
+	// }
+	
+	snake.setPosition(mx, my);
+	snake.setRadius(snakeSize);
+	snake.setPointCount(snakePoints);
+	snake.setFillColor(color);
+	snake.setOrigin(snakeSize, snakeSize);
+	//shape.setTexture(&snakeTexture);
+	
+       	snakeTail();
+        
     }
 
-    // Looking into using ternary to construct tail from vector of snakes
-    // void createSnakeTail(int nodes)
-    // {
-    //     for(int i = 0; i++; i < nodes)
-    // 	    {
-    // 		//add snake node to list and set each angle to follow the previous node
-    // 	    }
-    // }
-    
+    // Call to update properties of object each frame
     void update(CircleShape& target)
     {
-	shape.setRotation(util.angleToPointAt(shape,target,0.f));
+	snake.setRotation(util.angleToPointAt(snake,target,-90.f));
+    }
+
+    // Call to draw the updated property values each frame
+    void draw(RenderWindow& window)
+    {
+	// Draw a single snake node
+	window.draw(snake);
+	
+	// Draw each other snake node to create a tail
+	for(int i = 0; i < numberOfNodes; i++)
+	{
+	    // cout << "drawing stuff..." << "\n";
+	    window.draw(snakeNodes[i]);
+	}
     }
 };
     
@@ -91,15 +142,11 @@ int main()
 
     // Constructs and starts timer
     Clock clock;
-
     Color color;
 
-    // Creates a object in the center of the screen.
-    Snake snake{windowWidth / 2, windowHeight / 2, color.Green};
-    Snake otherSnake{windowWidth / 2, windowHeight - 200, color.Red};
-    Snake anotherSnake{windowWidth / 2, windowHeight - 150, color.Blue};
-    Snake theLastSnake{windowWidth / 2, windowHeight - 100, color.White};
-    
+    // Creates a snake object in the center of the screen.
+    int snakeNodes = 5;
+    Snake snake{windowWidth / 2, windowHeight / 2, color.Green, snakeNodes};
 
     float deltaTime;
 
@@ -109,31 +156,27 @@ int main()
 	Time elapsed = clock.restart();
 	deltaTime = elapsed.asSeconds();
 
-	cout << deltaTime << "\n";
+	//cout << deltaTime << "\n";	
 
-	
-	// Update list
-	otherSnake.update(snake.shape);
-	anotherSnake.update(otherSnake.shape);
-	theLastSnake.update(anotherSnake.shape);
-	
+	// Meh, an event
         Event event;
 
 	// Listens for close event to close the window.
         while (window.pollEvent(event))
         {
             if (event.type == Event::Closed)
-                window.close();
+	    {
+		window.close();
+	    }
         }
 
-	
+	// Clear the window each frame
         window.clear();
 	
 	// List of objects to draw
-        window.draw(snake.shape);
-	window.draw(otherSnake.shape);
-	window.draw(anotherSnake.shape);
-	window.draw(theLastSnake.shape);
+	snake.draw(window);
+
+	// Display the screen
         window.display();
     }
 
